7c1f5fc7329b37794163adf17fb4e20908e5cf2d
Matthew Abbott <matt@ingeniumsoftware.dev>
2021-10-19 20:27:22 +0100

    Ensure type becomes shared source

--- src/SharedSource.cs
+++ src/SharedSource.cs
@@ -3,42 +3,42 @@ using System;
 /// <summary>
 /// Provides utility methods for validating method arguments.
 /// </summary>
-public static class Ensure
+static class Ensure
 {
-    /// <summary>
-    /// Validates the given parameter value is not null.
-    /// </summary>
-    /// <typeparam name="T">The value type.</typeparam>
-    /// <param name="value">The value.</param>
-    /// <param name="paramName">The parameter name.</param>
-    /// <returns>The value</returns>
-    public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
-        where T : notnull
-    {
-        if (value is null)
-        {
-            throw new ArgumentNullException(paramName);
-        }
+	/// <summary>
+	/// Validates the given parameter value is not null.
+	/// </summary>
+	/// <typeparam name="T">The value type.</typeparam>
+	/// <param name="value">The value.</param>
+	/// <param name="paramName">The parameter name.</param>
+	/// <returns>The value</returns>
+	public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
+			where T : notnull
+	{
+		if (value is null)
+		{
+			throw new ArgumentNullException(paramName);
+		}
 
-        return value;
-    }
+		return value;
+	}
 
-    /// <summary>
-    /// Validates the given string parameter value is not an empty string.
-    /// </summary>
-    /// <param name="value">The value.</param>
-    /// <param name="paramName">The parameter name.</param>
-    /// <returns>The value.</returns>
-    public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
-    {
-        if (value is not { Length: > 0 })
-        {
-            throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
-        }
+	/// <summary>
+	/// Validates the given string parameter value is not an empty string.
+	/// </summary>
+	/// <param name="value">The value.</param>
+	/// <param name="paramName">The parameter name.</param>
+	/// <returns>The value.</returns>
+	public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
+	{
+		if (value is not { Length: > 0 })
+		{
+			throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
+		}
 
-        return value;
-    }
+		return value;
+	}
 }
 
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-public sealed class ValidatedNotNullAttribute : Attribute { }
+sealed class ValidatedNotNullAttribute : Attribute { }
