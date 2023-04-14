0fb44d981060e96b01d38220ecb21e01d8e880b8
Matthew Abbott <matt@ingeniumsoftware.dev>
2023-04-14 12:26:11 +0100

    feat: Target net7.0

--- src/SharedSource.cs
+++ src/SharedSource.cs
@@ -6,39 +6,39 @@ using System;
 [SkipCodeCoverage]
 internal static class Ensure
 {
-	/// <summary>
-	/// Validates the given parameter value is not null.
-	/// </summary>
-	/// <typeparam name="T">The value type.</typeparam>
-	/// <param name="value">The value.</param>
-	/// <param name="paramName">The parameter name.</param>
-	/// <returns>The value</returns>
-	public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
-			where T : notnull
+  /// <summary>
+  /// Validates the given parameter value is not null.
+  /// </summary>
+  /// <typeparam name="T">The value type.</typeparam>
+  /// <param name="value">The value.</param>
+  /// <param name="paramName">The parameter name.</param>
+  /// <returns>The value</returns>
+  public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
+		  where T : notnull
+  {
+	if (value is null)
 	{
-		if (value is null)
-		{
-			throw new ArgumentNullException(paramName);
-		}
-
-		return value;
+	  throw new ArgumentNullException(paramName);
 	}
 
-	/// <summary>
-	/// Validates the given string parameter value is not an empty string.
-	/// </summary>
-	/// <param name="value">The value.</param>
-	/// <param name="paramName">The parameter name.</param>
-	/// <returns>The value.</returns>
-	public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
-	{
-		if (value is not { Length: > 0 })
-		{
-			throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
-		}
+	return value;
+  }
 
-		return value;
+  /// <summary>
+  /// Validates the given string parameter value is not an empty string.
+  /// </summary>
+  /// <param name="value">The value.</param>
+  /// <param name="paramName">The parameter name.</param>
+  /// <returns>The value.</returns>
+  public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
+  {
+	if (value is not { Length: > 0 })
+	{
+	  throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
 	}
+
+	return value;
+  }
 }
 
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false), SkipCodeCoverage]
