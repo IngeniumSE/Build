e321661d85684c090211770d0aeabd4beba445f2
Matthew Abbott <matt@ingeniumsoftware.dev>
2024-02-12 13:48:37 +0000

    wip: Add MemberNotNullWhenAttribute to help compilation against older targets

--- src/SharedSource.cs
+++ src/SharedSource.cs
@@ -1,44 +1,42 @@
-using System;
-
 /// <summary>
 /// Provides utility methods for validating method arguments.
 /// </summary>
 [SkipCodeCoverage]
 internal static class Ensure
 {
-  /// <summary>
-  /// Validates the given parameter value is not null.
-  /// </summary>
-  /// <typeparam name="T">The value type.</typeparam>
-  /// <param name="value">The value.</param>
-  /// <param name="paramName">The parameter name.</param>
-  /// <returns>The value</returns>
-  public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
-		  where T : notnull
-  {
-	if (value is null)
+	/// <summary>
+	/// Validates the given parameter value is not null.
+	/// </summary>
+	/// <typeparam name="T">The value type.</typeparam>
+	/// <param name="value">The value.</param>
+	/// <param name="paramName">The parameter name.</param>
+	/// <returns>The value</returns>
+	public static T IsNotNull<T>([ValidatedNotNull] T? value, string paramName)
+			where T : notnull
 	{
-	  throw new ArgumentNullException(paramName);
+		if (value is null)
+		{
+			throw new ArgumentNullException(paramName);
+		}
+
+		return value;
 	}
 
-	return value;
-  }
-
-  /// <summary>
-  /// Validates the given string parameter value is not an empty string.
-  /// </summary>
-  /// <param name="value">The value.</param>
-  /// <param name="paramName">The parameter name.</param>
-  /// <returns>The value.</returns>
-  public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
-  {
-	if (value is not { Length: > 0 })
+	/// <summary>
+	/// Validates the given string parameter value is not an empty string.
+	/// </summary>
+	/// <param name="value">The value.</param>
+	/// <param name="paramName">The parameter name.</param>
+	/// <returns>The value.</returns>
+	public static string IsNotNullOrEmpty([ValidatedNotNull] string? value, string paramName)
 	{
-	  throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
-	}
+		if (value is not { Length: > 0 })
+		{
+			throw new ArgumentException($"The parameter '{paramName}' must be a non-empty value.");
+		}
 
-	return value;
-  }
+		return value;
+	}
 }
 
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false), SkipCodeCoverage]
@@ -48,3 +46,8 @@ internal sealed class ValidatedNotNullAttribute : Attribute
 [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = false), SkipCodeCoverage]
 internal sealed class SkipCodeCoverageAttribute : Attribute
 { }
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+#pragma warning disable CS9113 // Parameter is unread.
+internal sealed class MemberNotNullWhen(bool value, string propertyName) : Attribute { }
+#pragma warning restore CS9113 // Parameter is unread.
